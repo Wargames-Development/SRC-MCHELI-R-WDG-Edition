02e40198eaa40c3f51d796abfd783afddc65bf97
Glac <154849043+RhysHopkins04@users.noreply.github.com>
2025-12-06 02:26:59 +0000

    Updated version of the mod's build after merging all to figure it

--- src/main/java/mcheli/MCH_MOD.java
+++ src/main/java/mcheli/MCH_MOD.java
@@ -81,7 +81,7 @@ public class MCH_MOD {
     public static final String MOD_CH = "MCHeli_CH";
     public static final PacketHandler newPacketHandler = new PacketHandler();
     public static final MCH_EntityInfoManager entityInfoManager = new MCH_EntityInfoManager();
-    public static String VER = "0.1.4_X6";
+    public static String VER = "0.1.4_X7";
     @Instance("mcheli")
     public static MCH_MOD instance;
     @SidedProxy(
